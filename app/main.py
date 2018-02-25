5267b90b4a4d0857a8c8aa49a4a15d7466afa2e6
terahn <terahnharrison@gmail.com>
2018-02-24 23:46:28 -0800

    fixes

--- app/main.py
+++ app/main.py
@@ -16,9 +16,9 @@ def nextMove(data):
     move_y = my_y - food_y
 
     if (move_y > 0):
-        return 'down'
-    elif (move_y < 0):
         return 'up'
+    elif (move_y < 0):
+        return 'down'
     elif (move_x > 0):
         return 'left'
     else:
